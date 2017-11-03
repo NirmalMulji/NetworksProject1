75ce01abd2e6d725bb13a6ca4ed81ba4ea204e80
NirmalMulji <nirmalmulji@gmail.com>
2017-11-03 02:16:50 -0500

    changed

--- myserver.py
+++ myserver.py
@@ -3,9 +3,13 @@ from helper import *
 SUT_IP = gethostname()
 SUT_port = 12345
 
+serverName = "paris.cs.utexas.edu"
+serverPort = 35604
+serverIP = gethostbyname(serverName)
+
 # creates a UDP socket #
 sock = socket(AF_INET, SOCK_DGRAM)
-sock.bind((SUT_IP, SUT_port))
+sock.bind((serverIP, serverPort))
 
 while True:
     response, address = sock.recvfrom(2048)
