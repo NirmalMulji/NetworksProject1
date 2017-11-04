e7c5b55877bd3be4e37693329f412afcb2527b64
Nirmal Mulji <nirmal95@cs.utexas.edu>
2017-11-03 19:51:31 -0500

    Server returns error codes

--- helper.py
+++ helper.py
@@ -8,7 +8,64 @@ from socket import *;
 import sys;
 import math;
 
+lookup = {
+    111111111: 1234,
+    987654321: 4321,
+    325453877: 5113,
+    874397426: 2423,
+    544056088: 2343,
+    412324869: 4292,
+    402608790: 6290,
+    670338055: 1126,
+    453773539: 4213,
+    991926251: 8155,
+    976446819: 4076,
+    453439968: 5853,
+    184955947: 7620,
+    371749300: 3091,
+    372750813: 5516,
+    725846717: 4082,
+    954772946: 2594,
+    749682195: 1372,
+    293738284: 1067,
+    360332591: 7928,
+    718707857: 5881,
+    118181336: 8421,
+    233254549: 1939,
+    238262705: 2759,
+    958985134: 2672,
+    864657692: 4987,
+    110392619: 8312,
+    243172063: 6125,
+    584067155: 7780,
+    509850208: 2101,
+    872835726: 7755,
+    220526825: 7447,
+    405787039: 1893,
+    181176038: 3968,
+    107850102: 3920,
+    779121304: 4790,
+    180706750: 6841,
+    139691884: 3581,
+    697416533: 6224,
+    225234552: 4198,
+    945435863: 2615,
+    529503019: 7747,
+    405358883: 6472,
+    517407639: 7298,
+    836137273: 1030,
+    530528543: 6663,
+    459254350: 7997,
+    704752935: 1006,
+    569442788: 8628,
+    103267859: 1518,
+    975387379: 2120,
+    295889711: 5615}
 
+# returns the corresponding PO box given a SSN
+def dbase(SSN):
+    return lookup[SSN]
+    
 # pairs every 2 bytes in the sequence, creates array of 16 bit integers
 def pair2bytes(msg):
     pair = array("H")
@@ -90,87 +147,68 @@ def checkValidity(recvMessage, cookie, SSN, result):
 
     return False
 
-# returns the corresponding PO box given a SSN
-def dbase(SSN):
-
-    lookup = {
-            111111111: 1234,
-            987654321: 4321,
-            325453877: 5113,
-            874397426: 2423,
-            544056088: 2343,
-            412324869: 4292,
-            402608790: 6290,
-            670338055: 1126,
-            453773539: 4213,
-            991926251: 8155,
-            976446819: 4076,
-            453439968: 5853,
-            184955947: 7620,
-            371749300: 3091,
-            372750813: 5516,
-            725846717: 4082,
-            954772946: 2594,
-            749682195: 1372,
-            293738284: 1067,
-            360332591: 7928,
-            718707857: 5881,
-            118181336: 8421,
-            233254549: 1939,
-            238262705: 2759,
-            958985134: 2672,
-            864657692: 4987,
-            110392619: 8312,
-            243172063: 6125,
-            584067155: 7780,
-            509850208: 2101,
-            872835726: 7755,
-            220526825: 7447,
-            405787039: 1893,
-            181176038: 3968,
-            107850102: 3920,
-            779121304: 4790,
-            180706750: 6841,
-            139691884: 3581,
-            697416533: 6224,
-            225234552: 4198,
-            945435863: 2615,
-            529503019: 7747,
-            405358883: 6472,
-            517407639: 7298,
-            836137273: 1030,
-            530528543: 6663,
-            459254350: 7997,
-            704752935: 1006,
-            569442788: 8628,
-            103267859: 1518,
-            975387379: 2120,
-            295889711: 5615}
-    return lookup[SSN]
 
 def checkValidity2(recvMessage):
     # initialize validity checks to false
-    checkFirst = False
-    checkLab = False
-    checkVersion = False
-    checkCookie = False
-    checkSSN = False
-    checkChecksum = False
-    checkResult = False
+    # checkFirst = False
+    # checkLab = False
+    # checkVersion = False
+    # checkSSN = False
+    # checkChecksum = False
+    # checkResult = False
 
+    if not (recvMessage[0] == 356):
+        print "Syntax Error: not 356"
+        return 2 + (1 << 15)
+
+        # checkFirst = True
+
+    # checks if Lab is 1
+    if not (recvMessage[1] == 1):
+        print "Syntax Error: lab is not 1"
+        return 2 + (1 << 15)
+
+        # checkLab = True
+
+    # checks if version is 7
+    if not (recvMessage[2] == 7):
+        print "syntax error: version is not 7"
+        return 2 + (1 << 15)
+
+        # checkVersion = True
+
+    if not (recvMessage[4] in lookup):
+        print "Unknown SSN: ", recvMessage[4]
+        return 4 + (1 << 15)
+
+        # checkSSN = True
+
+    # verifies that "transaction outcome" bit of the result is not "1"
+    if not ((recvMessage[6] >> 15) < 1):
+        print "syntax error: transaction outcome bit is 1"
+        return 2 + (1 << 15)
+        # checkResult = True
+
+
+    # check validity of checksum. First pack into bytes
+    msg = bytearray()
+    msg.extend(pack("!HBBIIHH", recvMessage[0], recvMessage[1], recvMessage[2], recvMessage[3], recvMessage[4],
+                    recvMessage[5], recvMessage[6]))
     # re-compute checksum (and invert xor in computeChecksum())
-    reComputed = hex(computeChecksum(recvMessage) ^ 0xFFFF)
+
+    reComputed = hex(computeChecksum(msg) ^ 0xFFFF)
 
     # verify reComputed checksum is 0xFFFF
-    if (reComputed == "0xffff"):
-        checkChecksum = True
-        print reComputed
-    else:
+    if not (reComputed == "0xffff"):
         print "Checksum verification failure: ", reComputed
+        return 1 + (1 << 15)
+        # checkChecksum = True
 
+    # else:
+    #     print "Checksum verification failure: ", reComputed
+    
 
+    # if checkChecksum & checkFirst & checkLab & checkVersion & checkSSN & checkResult:
+    #     return True
 
-    if checkChecksum:
-        return True
-
-    return False
+    return 0
--- myserver.py
+++ myserver.py
@@ -11,8 +11,52 @@ serverIP = gethostbyname(serverName)
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.bind(('', SUT_port))
 
+res = (1<<14) + 356
+
 while True:
     response, address = sock.recvfrom(2048)
-    print response.decode()
 
+    recvMessage = unpack("!HBBIIHH", response)
+
+
+    if not (checkValidity2(recvMessage) == 0):
+        break
+
+
+    SSN = recvMessage[4]
+
+    PO = dbase(SSN)
+
+    msg = bytearray()
+	# pack 356, lab1, version7, cookie, SSN, checksum, and result to bytearray in network byte order
+    # msg.extend(pack("!HBBIIHH", res, recvMessage[1], recvMessage[2], recvMessage[3], recvMessage[4], recvMessage[5], PO))
+
+    msg.extend(pack("!H", res))
+    msg.extend(pack("!B", recvMessage[1]))
+    msg.extend(pack("!B", recvMessage[2]))
+    msg.extend(pack("!I", recvMessage[3]))
+    msg.extend(pack("!I", recvMessage[4]))
+    msg.extend(pack("!H", recvMessage[5]))
+    msg.extend(pack("!H", PO))
+
+
+    checksum = computeChecksum(msg)
+
+    newMessage = bytearray()
+    newMessage.extend(pack("!H", res))
+    newMessage.extend(pack("!B", recvMessage[1]))
+    newMessage.extend(pack("!B", recvMessage[2]))
+    newMessage.extend(pack("!I", recvMessage[3]))
+    newMessage.extend(pack("!I", recvMessage[4]))
+    newMessage.extend(pack("!H", checksum))
+    newMessage.extend(pack("!H", PO))
+    # newMessage.extend(pack("!HBBIIHH", res, recvMessage[1], recvMessage[2], recvMessage[3], recvMessage[4], checksum, PO))
+
+    sock.sendto(newMessage, address)
+
+    print PO
+
+
+# Receive a datagram with recvfrom(). Check it for correct version, format and checksum,
+# etc. If any of these is incorrect, return the appropriate result error code specified in Exercise 0.
 
